62c35530098c847405a57dbc9bde9376eefdb0a5
smaddin1 <123145992+smaddin1@users.noreply.github.com>
2023-10-30 17:53:08 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+
+function handleclick(){
+    var thisinnerhtml=this.innerHTML;
+    makesound(thisinnerhtml);
+    buttonanimation(thisinnerhtml);
+}
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonanimation(event.key);
+})
+function makesound(key){
+    switch (key){
+        case "w":
+            var sound=new Audio("sounds/crash.mp3");
+            sound.play();
+        break;
+        case "a":
+            var sound=new Audio("sounds/kick-bass.mp3");
+            sound.play();
+        break;
+        case "s":
+            var sound=new Audio("sounds/snare.mp3");
+            sound.play();
+        break;
+        case "d":
+            var sound=new Audio("sounds/tom-1.mp3");
+            sound.play();
+        break;
+        case "j":
+            var sound=new Audio("sounds/tom-2.mp3");
+            sound.play();
+        break;
+        case "k":
+            var sound=new Audio("sounds/tom-3.mp3");
+            sound.play();
+        break;
+        case "l":
+            var sound=new Audio("sounds/tom-4.mp3");
+            sound.play();
+        break;
+        default:
+            console.log(thisinnerhtml);
+    }
+}
+
+function buttonanimation(currentkey){
+    var activebutton=document.querySelector("."+currentkey);
+    activebutton.classList.add("pressed");
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    },100);
+}
+
